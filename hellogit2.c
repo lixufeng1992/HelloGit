b7c3ff985e979357da267539e4d5246bf337f76e
lixufeng <lixufeng1992@164.com>
2016-04-27 11:35:58 -0400

    commit hellogit2

---
+++ hellogit2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int
+main(int argc, char* argv[]){
+
+	printf("this is my second time to say hello to git\n");
+	return -1;//some error happened,return -1
+	exit(-1);//dead code 
+	
+}
